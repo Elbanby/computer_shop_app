0bd64f9c5f3a68ecfb1ad307b6cdc11a650208e0
omar Elbanby <omarelbanby@omars-iMac.local>
2018-07-27 18:28:08 -0400

    Fix merge conflicts

--- JS/handler.js
+++ JS/handler.js
@@ -1,4 +1,4 @@
-var n=1;
+var n = 1;
 
 function display(customer) {
   let name = customer.comptName;
@@ -8,21 +8,15 @@ function display(customer) {
   let email = customer.compEmail;
 
   //To dispaly the invoices
-  for (let i = 0 ; i < customer.invoice.length ; i++) {
-      let invoice = customer.invoice[i];
-      //console.log();
+  for (let i = 0; i < customer.invoice.length; i++) {
+    let invoice = customer.invoice[i];
+    //console.log();
   }
 }
 
+$(document).ready(() => {
 
-$(document).ready(()=>{
-
-  $.ajax({
-    type: "GET",
-    url: "./JSON/customers.json",
-    dataType: "json",
-    success: parseCutomers
-  });
+  $.ajax({type: "GET", url: "./JSON/customers.json", dataType: "json", success: parseCutomers});
 
   function parseCutomers(JsonObj) {
     //Number of customers included in the JSON file
@@ -30,74 +24,48 @@ $(document).ready(()=>{
     //Customer is an array of of cusotmers
     let customer = JsonObj.customer;
 
+    for (let i = 0; i < numCustomers; i++) {
+      let id = customer[i].compId;
 
+      $("#customerHNav").append("<section class='list ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow' id='p" + n + "'" + '<div>' + customer[i].comptName + '</div>' + "</section><br />");
 
-    for (let i = 0 ; i < numCustomers ; i++) {
-        let id = customer[i].compId;
+      //PLEASE WORK ON PARSING HERE
+      //PLEASE WORK ON PARSING HERE
+      //PLEASE WORK ON PARSING HERE
+      $("#customerHNav").append("<article id='d" + n + "'>" + "<p>work on the parsing later</p>" + "</article><br />");
 
-
-           $("#customerHNav").append("<section class='list ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow' id='p"+n+"'" +
-           '<div>' + customer[i].comptName + '</div>'  + "</section><br />");
-
-           //PLEASE WORK ON PARSING HERE
-           //PLEASE WORK ON PARSING HERE
-          //PLEASE WORK ON PARSING HERE
-           $("#customerHNav").append("<article id='d"+n+"'>" + "<p>work on the parsing later</p>"+  "</article><br />");
-
-           $("#d" + n).hide();			// hide the content div
-           checkDisplay(n);			// check to see if the company section is clicked
-           n++;
-
-
-        $('#customerHNav').append( '<div id="cust_' + id + '">' + '<a href="#custInfo_'+ id +
-         '" class="ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow">' +
-           customer[i].comptName + ' ('+ id +')' +'</a></div>');
-
+      $("#d" + n).hide(); // hide the content div
+      checkDisplay(n); // check to see if the company section is clicked
+      n++;
     }
   }
 
-  $.ajax({
-    type: "GET",
-    url: "./JSON/product.json",
-    dataType: "json",
-    success: parseProducts
-  });
+  $.ajax({type: "GET", url: "./JSON/product.json", dataType: "json", success: parseProducts});
 
   function parseProducts(JsonObj) {
     let products = JsonObj.product;
 
-    for (let i = 0 ; i < products.length ; i++) {
+    for (let i = 0; i < products.length; i++) {
 
+      $("#productHNav").append("<section class='list ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow' id='p" + n + "'" + '<div>ID: ' + products[i].prodId + ': </div>' + products[i].prodDiscr + ' (' + products[i].prodAmt + ')' + "</section><br />");
 
-         $("#productHNav").append("<section class='list ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow' id='p"+n+"'" +
-         '<div>ID: ' + products[i].prodId + ': </div>'  +
-         products[i].prodDiscr + ' ('+ products[i].prodAmt +')'+ "</section><br />");
+      //PLEASE WORK ON PARSING HERE
+      //PLEASE WORK ON PARSING HERE
+      //PLEASE WORK ON PARSING HERE
+      // create a list of invoices/content under each product
+      $("#productHNav").append("<article id='d" + n + "'>" + "<p>work on the parsing later</p>" + "</article><br />");
 
-          //PLEASE WORK ON PARSING HERE
-          //PLEASE WORK ON PARSING HERE
-         //PLEASE WORK ON PARSING HERE
-         // create a list of invoices/content under each product
-         $("#productHNav").append("<article id='d"+n+"'>" + "<p>work on the parsing later</p>"+  "</article><br />");
-
-         $("#d" + n).hide();			// hide the content div
-         checkDisplay(n);			// check to see if the product section is clicked
-         n++;
-
-      //productHNav
-      $('#productHNav').append( '<div id="prod_' + products[i].prodId + '">' + '<a href="#custInfo_'+ products[i].prodId +
-       '" class="ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow">' +
-         products[i].prodDiscr + ' ('+ products[i].prodAmt +')' +'</a></div>');
-
+      $("#d" + n).hide(); // hide the content div
+      checkDisplay(n); // check to see if the product section is clicked
+      n++;
     }
   }
 
-
-
-});//Document ready end
+}); //Document ready end
 
 function checkDisplay(n) {
-  $("#p"+n).click(function(){					// which product name section is clicked
-    $("article:not([id$='"+n+"'])").hide("slow");	// hide all that do not end in clicked section ($=)
-    $("#d"+n).toggle("fast");				// toggle (show/hide) content div
+  $("#p" + n).click(function() { // which product name section is clicked
+    $("article:not([id$='" + n + "'])").hide("slow"); // hide all that do not end in clicked section ($=)
+    $("#d" + n).toggle("fast"); // toggle (show/hide) content div
   });
 }
